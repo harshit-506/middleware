6d51ad67ebcf6b1582b28322298808b2aab4a905
Harshit Singh <122781586+harshit-506@users.noreply.github.com>
2023-09-10 16:43:56 +0000

    day2

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ const app=express();
 
 //use absolute path for accessing static html
 app.use(express.static(path.join(path.resolve(),"public")))
+app.use(express.urlencoded({extended:true}));
+//using middleware 
 app.set("view engine","ejs");
-app.get('/',(req,res)=>{
-    res.render("index",{name:'Harshit'});
+const user=[];
+app.get("/",(req,res)=>{
+    res.render("index",{name:"singh"});
+})
+
+app.get('/success',(req,res)=>{
+    res.render("success");
    
+})
+app.post('/contact',(req,res)=>{
+     // u can access name as well
+    user.push({usename:req.body.name,email:req.body.email})
+
+    res.redirect("/success")
+})
+// it is temperory data
+app.get("/user",(req,res)=>{
+    res.json({
+        user,
+    })
 })
 
 app.listen(3000,()=>{
